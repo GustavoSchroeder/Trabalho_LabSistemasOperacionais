009186b608b97a5550ec6248d7da872389380bc4
Gustavo Lazarotto Schroeder <guto_schroeder@hotmail.com>
2015-04-20 10:04:47 -0300

    Update auto_test
    Várias mudanças de implementação

--- auto_test/auto_test.sh
+++ auto_test/auto_test.sh
@@ -28,27 +28,38 @@ function compilaTestaArquivo(){
   contMais=1
   contPastas=$(ls ./ | wc -l);
   registroDeErros=" - Erros -  \n"
+  extensaoc=".c"
+  extensaoo=".o"
 
 	while test $contPasta -gt "0"; do
-           if(! -e "$PROGRAMAC" "$inicio" "$contNumFin" ".c") #testa se arquivo existe
+		#Todos os caminhos feitos usando variáveis
+	   caminho=$inicio$contNumFin$extensaoc
+	   caminho01=$inicio$contNumFin
+	   caminho_saida=$TESTCASES_DIR$contNumFim$saida
+	   caminho_entrada=$TESTCASES_DIR$contpasta$entrada
+	   
+           if [ ! -e "$PROGRAMAC" "$inicio" "$contNumFin" ".c" ] then #testa se arquivo existe
 	     echo "Arquivo não existe"
 	     $contNumFin=$((contNumFin-contMais)) #caso nao exista já passa esse
 	   else
 	   #caso existir compila o programa
-		 caminho="$inicio" "$contNumFin" ".c" 
-		 caminho01="$inicio" "$contNumFin"
-		 if gcc "$caminho" -o "$caminho1"  then 
+		 if [ gcc "$caminho" -o "$caminho1" ] then 
 	   		##testa se o gcc falhou, caso tenha falhado o programa C já está errado.
 	   		$registroDeErros="Código contém erro: Programa número $contNumFin (Erro de compilação) \n"
 		 	$contNumFin=$((contNumFin-contMais))
 		   else
-			 $contNumFin=$((contNumFin-contMais)) #incrementa contador
-	           	solucao=./$caminho01.o < ./$contpasta$entrada #executa o arquivo mudando a entrada padrão
+			$contNumFin=$((contNumFin-contMais)) #incrementa contador
+	           	solucao=$caminho_saida < $caminho_entrada #executa o arquivo mudando a entrada padrão
 	 
-		 	if "$TESTCASES_DIR$contNumFim$saida" -ne "$solucao" then
+		 	if [ "$caminho_saida" -ne "$solucao" ] then  # Se resultado for diferente da solução está errado
 				$registroDeErros="Código contém erro: Programa número $contNumFin (Erro na saída) \n"
 				$contNumFin=$((contNumFin-contMais))
 			fi
 		fi
 	fi
+	caminho=" "
+   	caminho01=" "
+	caminho_saida=" "
+	caminho_entrada=" "
+	done
 }
