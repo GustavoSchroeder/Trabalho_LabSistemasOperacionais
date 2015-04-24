b6b53c02eebbf1f552394c85ea1b062f4de97510
Gustavo Lazarotto Schroeder <guto_schroeder@hotmail.com>
2015-04-24 08:21:04 -0300

    Update auto_test
    Feito novo Loop para verificar os casos de teste, não apenas quantos programas C Existem, mas também quantos casos de teste existem.

--- auto_test/auto_test.sh
+++ auto_test/auto_test.sh
@@ -26,8 +26,9 @@ function compilaTestaArquivo(){
   entrada="_INPUT"
   saida="_OUTPUT"
   contNumFin=00
+  contNumSoluc=00
   contMais=1
-  contPastas=$(ls ./ | wc -l);
+  contPastas=$(ls $PROGRAMAC | wc -l);
   registroDeErros=" - Erros -  \n"
   extensaoc=".c"
   extensaoo=".o"
@@ -36,26 +37,29 @@ function compilaTestaArquivo(){
 		#Todos os caminhos feitos usando variáveis
 	   caminho=$inicio$contNumFin$extensaoc
 	   caminho01=$inicio$contNumFin
-	   caminho_saida=$TESTCASES_DIR$contNumFim$saida
-	   caminho_entrada=$TESTCASES_DIR$contpasta$entrada
-	   
-           if [ ! -e "$PROGRAMAC" "$inicio" "$contNumFin" ".c" ] then #testa se arquivo existe
-	     echo "Arquivo não existe"
+	   caminho_saida=$TESTCASES_DIR$contNumSoluc$saida
+	   caminho_entrada=$TESTCASES_DIR$contNumSoluc$entrada
+	  if [ ! -e "$PROGRAMAC" "$inicio" "$contNumFin" ".c" ] then #testa se arquivo existe
+	     echo "Código $contNumFin não foi entregue."
 	     $contNumFin=$((contNumFin-contMais)) #caso nao exista já passa esse
 	   else
 	   #caso existir compila o programa
+	   	conta_arquivos=$((ls $PROGRAMAC/*.$caminho01 | wc -l)/2)
+	   		while test $conta_arquivos -gt "0"; do
 		 if [ gcc "$caminho" -o "$caminho1" ] then 
 	   		##testa se o gcc falhou, caso tenha falhado o programa C já está errado.
 	   		$registroDeErros="Código contém erro: Programa número $contNumFin (Erro de compilação) \n"
 		 	$contNumFin=$((contNumFin-contMais))
 		   else
-			$contNumFin=$((contNumFin-contMais)) #incrementa contador
-	           	solucao=$caminho_saida < $caminho_entrada #executa o arquivo mudando a entrada padrão
+		        	$contNumFin=$((contNumFin-contMais)) #incrementa contador
+	                	solucao=$caminho_saida < $($caminho_entrada$conta_arquivos) #executa o arquivo mudando a entrada padrão
 	 
 		 	if [ "$caminho_saida" -ne "$solucao" ] then  # Se resultado for diferente da solução está errado
 				$registroDeErros="Código contém erro: Programa número $contNumFin (Erro na saída) \n"
 				$contNumFin=$((contNumFin-contMais))
 			fi
+			solucao=" " #zera a variável
+		    done
 		fi
 	fi
 	caminho=" "
